e331c44cf5caedca07553934ebc018e219825cf7
Jazeb_Javed <jazebjaved52@gmail.com>
2024-10-17 02:00:59 +0500

    feat/configure text editing controllers

--- lib/views/barberScreens/registeration_barber.dart
+++ lib/views/barberScreens/registeration_barber.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'dart:typed_data';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+
+import 'package:trim_time/providers/sample_provider.dart';
+
+import 'package:trim_time/controller/upload_image.dart';
 
 class BarberRegistrationPage extends StatefulWidget {
   BarberRegistrationPage({
@@ -29,8 +38,89 @@ class BarberRegistrationPage extends StatefulWidget {
 }
 
 class _BarberRegistrationPageState extends State<BarberRegistrationPage> {
+  final isClient = false;
+  // bool _isLoading = true;
+
+  // late String genderDropDownValue;
+  // late int openingTimeDropDownValue;
+  // late int closingTimeDropDownValue;
+  late bool? isProvidingHaircut;
+  late bool? isProvidingShave;
+  late bool? isProvidingBeardTrim;
+  late bool? isProvidingMassage;
+
+  // late Map<String, dynamic> localData;
+
+  // LocalStorageModel? localStorageData;
+
+  Uint8List? _image;
+  _loadData() {
+    // await Future.delayed(const Duration(seconds: 2));
+
+    // localData = await getDataFromLocalStorage();
+
+    // print('------------>localData in barber registration page----> $localData');
+
+    // final _services = localData['userData']['services'];
+
+    // setState(() {
+    isProvidingHaircut = widget.services['1']['isProviding'];
+    isProvidingShave = widget.services['2']['isProviding'];
+    isProvidingBeardTrim = widget.services['3']['isProviding'];
+    isProvidingMassage = widget.services['4']['isProviding'];
+    // genderDropDownValue = widget.gender;
+    // openingTimeDropDownValue = widget.openingTime;
+    // closingTimeDropDownValue = widget.closingTime;
+    // _isLoading = false;
+    // });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
+    AppProvider appProvider = Provider.of<AppProvider>(context, listen: false);
+
+    selectImage() async {
+      Uint8List? img = await pickImage(ImageSource.gallery);
+      if (img == null) return;
+
+      log('image file size: ${img.lengthInBytes}');
+
+      _image = img;
+      appProvider.notifyListeners();
+    }
+
+    appProvider.isProvidingHaircut = widget.services['1']['isProviding'];
+    appProvider.isProvidingShave = widget.services['2']['isProviding'];
+    appProvider.isProvidingBeardTrim = widget.services['3']['isProviding'];
+    appProvider.isProvidingMassage = widget.services['4']['isProviding'];
+
+    TextEditingController fullNameController =
+        TextEditingController(text: widget.fullName);
+    TextEditingController nickNameController = TextEditingController();
+
+    TextEditingController emailController =
+        TextEditingController(text: widget.email);
+
+    TextEditingController phoneNumberController =
+        TextEditingController(text: widget.phoneNumber);
+    TextEditingController haircutPriceController =
+        TextEditingController(text: widget.services['1']['price'].toString());
+    TextEditingController beardTrimPriceController =
+        TextEditingController(text: widget.services['2']['price'].toString());
+    TextEditingController shavePriceController =
+        TextEditingController(text: widget.services['3']['price'].toString());
+    TextEditingController massagePriceController =
+        TextEditingController(text: widget.services['4']['price'].toString());
+
+    final shopNameController = TextEditingController();
+    final shopAddressController = TextEditingController();
+    final shopPhoneNumberController = TextEditingController();
     return Container();
   }
 }
