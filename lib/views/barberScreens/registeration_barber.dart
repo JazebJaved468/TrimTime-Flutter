c8f8d5768ce86968192d0399b6f71c28635c9479
Jazeb_Javed <jazebjaved52@gmail.com>
2024-10-17 02:15:13 +0500

    Registration Functionality

--- lib/views/barberScreens/registeration_barber.dart
+++ lib/views/barberScreens/registeration_barber.dart
@@ -6,6 +6,7 @@ import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:trim_time/colors/custom_colors.dart';
+import 'package:trim_time/controller/date_time.dart';
 import 'package:trim_time/controller/firestore.dart';
 
 import 'package:trim_time/providers/sample_provider.dart';
@@ -446,6 +447,252 @@ class _BarberRegistrationPageState extends State<BarberRegistrationPage> {
                     ),
                   ],
                 ),
+                Consumer<AppProvider>(builder: (context, provider, child) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: CustomColors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (appProvider.isProvidingShave! ||
+                          appProvider.isProvidingShave! ||
+                          appProvider.isProvidingBeardTrim! ||
+                          appProvider.isProvidingMassage!) {
+                        if (fullNameController.text.trim().isNotEmpty &&
+                            phoneNumberController.text.trim().isNotEmpty &&
+                            shopNameController.text.trim().isNotEmpty &&
+                            shopAddressController.text.trim().isNotEmpty &&
+                            shopPhoneNumberController.text.trim().isNotEmpty &&
+                            haircutPriceController.text.trim().isNotEmpty &&
+                            shavePriceController.text.trim().isNotEmpty &&
+                            beardTrimPriceController.text.trim().isNotEmpty &&
+                            massagePriceController.text.trim().isNotEmpty) {
+                          provider.setSaveClientProfileCIP(true);
+
+                          if (_image != null) {
+                            final photo = await appProvider
+                                .updateUserProflileImage(
+                                    userId: appProvider
+                                        .localDataInProvider['userData']['uid'],
+                                    isClient: appProvider
+                                            .localDataInProvider['userData']
+                                        ['isClient'],
+                                    file: _image);
+
+                            await updateUserRegistrationDataInFirestore(
+                                userId: widget.uid,
+                                isClient: isClient,
+                                data: {
+                                  'isRegistered': true,
+                                  'name': fullNameController.text,
+                                  'nickName': nickNameController.text,
+                                  'email': emailController.text,
+                                  'phoneNumber': phoneNumberController.text,
+                                  'gender':
+                                      appProvider.barberGender.toLowerCase(),
+                                  'shopName': shopNameController.text,
+                                  'shopAddress': shopAddressController.text,
+                                  'openingTime': appProvider.barberOpeningTime,
+                                  'closingTime': appProvider.barberClosingTime,
+                                  'shopPhoneNumber':
+                                      shopPhoneNumberController.text,
+                                  'availability': generate7DaysSlots(
+                                      DateTime.now(),
+                                      appProvider.barberOpeningTime,
+                                      appProvider.barberClosingTime),
+                                  'services': {
+                                    '1': {
+                                      'serviceId': '1',
+                                      'isProviding':
+                                          appProvider.isProvidingHaircut,
+                                      'price': int.parse(
+                                          haircutPriceController.text),
+                                    },
+                                    '2': {
+                                      'serviceId': '2',
+                                      'isProviding':
+                                          appProvider.isProvidingShave,
+                                      'price':
+                                          int.parse(shavePriceController.text),
+                                    },
+                                    '3': {
+                                      'serviceId': '3',
+                                      'isProviding':
+                                          appProvider.isProvidingBeardTrim,
+                                      'price': int.parse(
+                                          beardTrimPriceController.text),
+                                    },
+                                    '4': {
+                                      'serviceId': '4',
+                                      'isProviding':
+                                          appProvider.isProvidingMassage,
+                                      'price': int.parse(
+                                          massagePriceController.text),
+                                    },
+                                  },
+                                  'photoURL': photo == ''
+                                      ? appProvider
+                                              .localDataInProvider['userData']
+                                          ['photoURL']
+                                      : photo,
+                                });
+                          } else {
+                            await updateUserRegistrationDataInFirestore(
+                                userId: widget.uid,
+                                isClient: isClient,
+                                data: {
+                                  'isRegistered': true,
+                                  'name': fullNameController.text,
+                                  'nickName': nickNameController.text,
+                                  'email': emailController.text,
+                                  'phoneNumber': phoneNumberController.text,
+                                  'gender':
+                                      appProvider.barberGender.toLowerCase(),
+                                  'shopName': shopNameController.text,
+                                  'shopAddress': shopAddressController.text,
+                                  'openingTime': appProvider.barberOpeningTime,
+                                  'closingTime': appProvider.barberClosingTime,
+                                  'shopPhoneNumber':
+                                      shopPhoneNumberController.text,
+                                  'availability': generate7DaysSlots(
+                                      DateTime.now(),
+                                      appProvider.barberOpeningTime,
+                                      appProvider.barberClosingTime),
+                                  'services': {
+                                    '1': {
+                                      'serviceId': '1',
+                                      'isProviding':
+                                          appProvider.isProvidingHaircut,
+                                      'price': int.parse(
+                                          haircutPriceController.text),
+                                    },
+                                    '2': {
+                                      'serviceId': '2',
+                                      'isProviding':
+                                          appProvider.isProvidingShave,
+                                      'price':
+                                          int.parse(shavePriceController.text),
+                                    },
+                                    '3': {
+                                      'serviceId': '3',
+                                      'isProviding':
+                                          appProvider.isProvidingBeardTrim,
+                                      'price': int.parse(
+                                          beardTrimPriceController.text),
+                                    },
+                                    '4': {
+                                      'serviceId': '4',
+                                      'isProviding':
+                                          appProvider.isProvidingMassage,
+                                      'price': int.parse(
+                                          massagePriceController.text),
+                                    },
+                                  },
+                                });
+                          }
+
+                          // await updateUserRegistrationDataInFirestore(
+                          //     userId: widget.uid,
+                          //     isClient: isClient,
+                          //     data: {
+                          //       'isRegistered': true,
+                          //       'name': fullNameController.text,
+                          //       'nickName': nickNameController.text,
+                          //       'email': emailController.text,
+                          //       'phoneNumber': phoneNumberController.text,
+                          //       'gender':
+                          //           appProvider.barberGender.toLowerCase(),
+                          //       'shopName': shopNameController.text,
+                          //       'shopAddress': shopAddressController.text,
+                          //       'openingTime': appProvider.barberOpeningTime,
+                          //       'closingTime': appProvider.barberClosingTime,
+                          //       'shopPhoneNumber':
+                          //           shopPhoneNumberController.text,
+                          //       'availability': generate7DaysSlots(
+                          //           DateTime.now(),
+                          //           appProvider.barberOpeningTime,
+                          //           appProvider.barberClosingTime),
+                          //       'services': {
+                          //         '1': {
+                          //           'serviceId': '1',
+                          //           'isProviding':
+                          //               appProvider.isProvidingHaircut,
+                          //           'price':
+                          //               int.parse(haircutPriceController.text),
+                          //         },
+                          //         '2': {
+                          //           'serviceId': '2',
+                          //           'isProviding': appProvider.isProvidingShave,
+                          //           'price':
+                          //               int.parse(shavePriceController.text),
+                          //         },
+                          //         '3': {
+                          //           'serviceId': '3',
+                          //           'isProviding':
+                          //               appProvider.isProvidingBeardTrim,
+                          //           'price': int.parse(
+                          //               beardTrimPriceController.text),
+                          //         },
+                          //         '4': {
+                          //           'serviceId': '4',
+                          //           'isProviding':
+                          //               appProvider.isProvidingMassage,
+                          //           'price':
+                          //               int.parse(massagePriceController.text),
+                          //         },
+                          //       },
+                          //     });
+
+                          await updateUserDataInLocalStorage(
+                              data: await getUserDataFromFirestore(
+                                  widget.uid, isClient));
+
+                          provider.setSaveClientProfileCIP(false);
+                          if (_image != null) {
+                            appProvider.setProfileImageInBytes(_image);
+                          }
+                          if (mounted) {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => BarberHomePage()),
+                            );
+                          }
+                        } else {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content:
+                                Text('Kindly fill all the required fields'),
+                          ));
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Please provide at least one service'),
+                        ));
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      child: Center(
+                        child: provider.saveClientProfileCIP
+                            ? const SpinKitFadingCircle(
+                                color: CustomColors.white,
+                                size: 26.0,
+                              )
+                            : Text(
+                                'Save Profile',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: CustomColors.white,
+                                ),
+                              ),
+                      ),
+                    ),
+                  );
+                })
               ],
             ),
           ),
