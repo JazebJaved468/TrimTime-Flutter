7d36ec70330fc0815502a4a1d4b073f5da2aeff4
Zohaib Ali <zohaib.ali236@gmail.com>
2024-10-20 00:59:52 +0500

    Added basic structure for managing barber slots

---
+++ lib/views/barberScreens/manage_slots.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:trim_time/colors/custom_colors.dart';
+import 'package:trim_time/components/CustomAppBar.dart';
+import 'package:trim_time/controller/firestore.dart';
+import 'package:trim_time/controller/local_storage.dart';
+import 'package:trim_time/controller/login.dart';
+import 'package:trim_time/providers/sample_provider.dart';
+import 'package:trim_time/views/sign_in/sign_in.dart';
+
+class ManageSlots extends StatefulWidget {
+  const ManageSlots({super.key, required this.day});
+
+  final String day;
+
+  @override
+  State<ManageSlots> createState() => _ManageSlotsState();
+}
+
+class _ManageSlotsState extends State<ManageSlots> {
+  final isClient = false;
+
+  _updateData(
+      {required String uid,
+      required Map<String, dynamic> barberAvailability}) async {
+    await updateBarberAvailabilityInFirestore(
+      barberId: uid,
+      data: barberAvailability,
+    );
+
+    await updateUserDataInLocalStorage(
+        data: await getUserDataFromFirestore(uid, isClient));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    AppProvider appProvider = Provider.of<AppProvider>(context, listen: false);
+
+    return Scaffold(
+        backgroundColor: CustomColors.gunmetal,
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: CustomColors.peelOrange,
+            onPressed: () async {
+              appProvider.setUpdateSlotsCIP(true);
+
+              await _updateData(
+                  uid: appProvider.uid,
+                  barberAvailability: appProvider.barberAvailability);
+
+              appProvider.setUpdateSlotsCIP(false);
+
+              if (mounted) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Slots updated successfully'),
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              }
+            },
+            child: Consumer<AppProvider>(
+              builder: (context, provider, child) {
+                return provider.updateSlotsCIP
+                    ? const SpinKitFadingCircle(
+                        color: CustomColors.charcoal,
+                        size: 26.0,
+                      )
+                    : const Icon(
+                        Icons.done_rounded,
+                        size: 28,
+                      );
+              },
+            )),
+        appBar: CustomAppBar(
+          title: '${DateFormat('EEEE').format(DateTime.parse(widget.day))}',
+          leftIcon: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: CustomColors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: appProvider.barberAvailability[widget.day] == null
+            ? const Center(
+                child: Text('No slots available for this day'),
+              )
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 70,
+                    )
+                  ],
+                ),
+              ));
+  }
+}
