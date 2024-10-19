3427a21ee10a09a4a25a544eed5739fa3dd11475
Jazeb_Javed <jazebjaved52@gmail.com>
2024-10-20 01:14:03 +0500

    feat: style days screen

--- lib/views/barberScreens/manage_days.dart
+++ lib/views/barberScreens/manage_days.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
+import 'package:trim_time/colors/custom_colors.dart';
+import 'package:trim_time/components/CustomAppBar.dart';
 import 'package:trim_time/controller/firestore.dart';
 import 'package:trim_time/controller/local_storage.dart';
 import 'package:trim_time/controller/login.dart';
@@ -11,6 +14,7 @@ import 'package:trim_time/views/sign_in/sign_in.dart';
 class ManageDays extends StatefulWidget {
   const ManageDays({super.key});
 
+  // final barberAvailability;
   @override
   State<ManageDays> createState() => _ManageDaysState();
 }
@@ -20,7 +24,7 @@ class _ManageDaysState extends State<ManageDays> {
   final isClient = false;
   bool _isLoading = true;
 
-   _loadData() async {
+  _loadData() async {
     localData = await getDataFromLocalStorage();
 
     setState(() {
@@ -40,9 +44,9 @@ class _ManageDaysState extends State<ManageDays> {
         data: await getUserDataFromFirestore(uid, isClient));
   }
 
-
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     _loadData();
   }
@@ -52,89 +56,162 @@ class _ManageDaysState extends State<ManageDays> {
     AppProvider appProvider = Provider.of<AppProvider>(context, listen: false);
 
     var days = appProvider.barberAvailability.keys.toList();
+    // Sorting days bcz firebase is not giving values in correct order
     days.sort((a, b) => DateTime.parse(a).compareTo(DateTime.parse(b)));
 
     return Scaffold(
-            floatingActionButton: FloatingActionButton(onPressed: () async {
-        appProvider.setUpdateDaysCIP(true);
+      backgroundColor: CustomColors.gunmetal,
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: CustomColors.peelOrange,
+          onPressed: () async {
+            appProvider.setUpdateDaysCIP(true);
 
-        await _updateData(
-            uid: appProvider.uid,
-            barberAvailability: appProvider.barberAvailability);
+            await _updateData(
+                uid: appProvider.uid,
+                barberAvailability: appProvider.barberAvailability);
 
-        appProvider.setUpdateDaysCIP(false);
+            appProvider.setUpdateDaysCIP(false);
 
-        if (mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Days updated successfully'),
-              duration: Duration(seconds: 1),
-            ),
-          );
-        }
-      }, child: Consumer<AppProvider>(
-        builder: (context, provider, child) {
-          return provider.updateDaysCIP
-              ? Container(
-                  width: 20.0,
-                  height: 20.0,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 1,
-                  ),
-                )
-              : const Icon(Icons.done);
-        },
-      )),
-      appBar: AppBar(
-        title: const Text('Manage Days'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              await signOut();
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => SignIn()),
-                  (Route route) => false);
+            if (mounted) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Days updated successfully'),
+                  duration: Duration(seconds: 1),
+                ),
+              );
+            }
+          },
+          child: Consumer<AppProvider>(
+            builder: (context, provider, child) {
+              return provider.updateDaysCIP
+                  ? const SpinKitFadingCircle(
+                      color: CustomColors.charcoal,
+                      size: 26.0,
+                    )
+                  : const Icon(
+                      Icons.done_rounded,
+                      size: 28,
+                    );
             },
-            icon: const Icon(Icons.logout),
+          )),
+      appBar: CustomAppBar(
+        title: 'Manage Days',
+        // rightIcon: IconButton(
+        //   onPressed: () async {
+        //     await signOut();
+        //     Navigator.of(context).pushAndRemoveUntil(
+        //         MaterialPageRoute(builder: (context) => SignIn()),
+        //         (Route route) => false);
+        //   },
+        //   icon: const Icon(Icons.logout),
+        // ),
+        leftIcon: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: CustomColors.white,
           ),
-        ],
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
       body: _isLoading
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : ListView.builder(
-              itemCount: appProvider.barberAvailability.keys.length,
-              itemBuilder: (context, index) {
-                var day = days[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ManageSlots(
-                          day: day,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Consumer<AppProvider>(
-                      builder: (context, provider, child) {
-                    return ListTile(
-                      title: Text(
-                          'Day : ${DateFormat('EEEE').format(DateTime.parse(day))}'),
-                      subtitle: Wrap(
-                        direction: Axis.vertical,
-                        children: [
-                          Text(
-                              'Date : ${DateFormat('d MMM').format(DateTime.parse(day))}'),
-                        ],
-                      ),
-                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                    );
-                  }),
-                );
-              }),
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: appProvider.barberAvailability.keys.length,
+                      itemBuilder: (context, index) {
+                        var day = days[index];
+
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ManageSlots(
+                                  day: day,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Consumer<AppProvider>(
+                              builder: (context, provider, child) {
+                            return Container(
+                              padding:
+                                  EdgeInsets.only(top: 10, bottom: 10, left: 4),
+                              decoration: BoxDecoration(
+                                color: CustomColors.charcoal,
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(
+                                  color: Colors.black38, // Border color
+                                  width: 1, // Border width
+                                ),
+                              ),
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 6, horizontal: 8),
+                              child: ListTile(
+                                  title: Text(
+                                    'Day : ${DateFormat('EEEE').format(DateTime.parse(day))}',
+                                    style: TextStyle(color: CustomColors.white),
+                                  ),
+                                  subtitle:
+                                      Wrap(direction: Axis.vertical, children: [
+                                    Text(
+                                      'Date : ${DateFormat('d MMM').format(DateTime.parse(day))}',
+                                      style:
+                                          TextStyle(color: CustomColors.white),
+                                    ),
+                                    Transform.scale(
+                                      scale: 0.8,
+                                      child: Switch(
+                                        trackOutlineWidth:
+                                            MaterialStatePropertyAll(1),
+                                        trackOutlineColor:
+                                            MaterialStatePropertyAll(
+                                                CustomColors.peelOrange),
+                                        activeColor: Colors.black,
+                                        activeTrackColor:
+                                            CustomColors.peelOrange,
+                                        inactiveThumbColor:
+                                            CustomColors.peelOrange,
+                                        inactiveTrackColor:
+                                            CustomColors.transparent,
+                                        value:
+                                            appProvider.barberAvailability[day]
+                                                ['isAvailable'],
+                                        onChanged:
+                                            appProvider.barberAvailability[day]
+                                                        ['hasBooking'] ??
+                                                    false
+                                                ? null
+                                                : (value) {
+                                                    provider
+                                                        .updateBarberDaysAvailability(
+                                                            day: day,
+                                                            value: value);
+                                                  },
+                                      ),
+                                    ),
+                                  ]),
+                                  trailing: Icon(
+                                      Icons.arrow_forward_ios_rounded,
+                                      color: CustomColors.white)),
+                            );
+                          }),
+                        );
+                      }),
+                  SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
+            ),
     );
   }
 }
